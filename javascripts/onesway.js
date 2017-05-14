6321d3392b412198f85d215da9c781deae47688c
Sinix512 <xusiming512@163.com>
2017-05-14 12:16:23 +0800

    Update onesway.js

--- javascripts/onesway.js
+++ javascripts/onesway.js
@@ -2,10 +2,23 @@ function Roll(min, max) {
 	var num = min + Math.floor(Math.random() * (max - min));
 	if(num == 0)
 	{
-		alert("不去");
+		document.getElementById('rollzone').innerHTML = document.getElementById('rollzone').innerHTML + "<br>维维要去";
 	}
 	else
 	{
-		alert("去");
+		document.getElementById('rollzone').innerHTML = document.getElementById('rollzone').innerHTML + "<br>维维不要去";
 	}
+}
+
+function clearContent() {
+	document.getElementById('rollzone').innerHTML = "";
+}
+
+function RollNum(min, max) {
+	var num = min + Math.floor(Math.random() * (max - min));
+	addRollRes(num);
+}
+
+function addRollRes(num){
+	document.getElementById('rollzone').innerHTML = document.getElementById('rollzone').innerHTML + "<br>维维扔出了" + num;
 }
